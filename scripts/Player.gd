ad3acdc71dce4e5ae31afa23c5d8e0fdf8e8fa6a
Luke Mohr <lukemohr@gmail.com>
2024-05-19 17:11:11 -0400

    alien attack 20240519
    progress up to and including Understanding Delta Time

---
+++ scripts/Player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var speed = 300
+
+func _physics_process(delta):
+	# Need to reset velocity each frame
+	velocity = Vector2(0,0)
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	move_and_slide()
+	
+	var screen_size = get_viewport_rect().size
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
