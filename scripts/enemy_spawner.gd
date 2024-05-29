5f7ab378a9d3698b5436add749cf56f2a3c5598d
lukemohr <lukemohr@gmail.com>
2024-05-28 20:05:14 -0400

    complete
    finished alien attack tutorial

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 signal enemy_spawned(enemy_instance)
+signal path_enemy_spawned(path_enemy_instance)
 var enemy_scene = preload("res://scenes/enemy.tscn")
 @onready var spawn_positions = $SpawnPositions
+var path_enemy_scene = preload("res://scenes/path_enemy.tscn")
 
 func spawn_enemy():
 	var spawn_positions_array = spawn_positions.get_children()
@@ -13,3 +15,10 @@ func spawn_enemy():
 
 func _on_timer_timeout():
 	spawn_enemy()
+
+func _on_path_enemy_timer_timeout():
+	spawn_path_enemy()
+	
+func spawn_path_enemy():
+	var path_enemy_instance = path_enemy_scene.instantiate()
+	emit_signal("path_enemy_spawned", path_enemy_instance)
--- scripts/game.gd
+++ scripts/game.gd
@@ -36,3 +36,8 @@ func _on_enemy_died():
 	enemy_hit_sound.play()
 	score += 100
 	hud.set_score_label(score)
+
+
+func _on_enemy_spawner_path_enemy_spawned(path_enemy_instance):
+	add_child(path_enemy_instance)
+	path_enemy_instance.enemy.connect("died", _on_enemy_died)
